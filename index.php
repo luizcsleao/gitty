8c6bf06389f37d82a877eae1b377307c49315016
Luiz Claudio da Silva Leao <luizcsleao@gmail.com>
2018-06-12 21:12:08 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+<form method="post" action="./p_login.php" id="formlogin" name="formlogin" >
+<fieldset id="fie">
+<legend>LOGIN</legend><br />
+<label>NOME : </label>
+<input type="text" name="login" id="login"  /><br />
+<label>SENHA :</label>
+<input type="password" name="senha" id="senha" /><br />
+<input type="submit" value="LOGAR    "  />
+</fieldset>
+</form>
+</body>
